7553196abbc2f90161b855c3f83bb2fa01e3a6b7
juanss25 <jsantoss@uni.pe>
2025-05-22 18:58:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -93,9 +93,6 @@ if uploaded_file:
                 pdf.set_auto_page_break(auto=True, margin=15)
                 pdf.add_page()
 
-                # Agregar logo si existe
-                if os.path.exists("logo_smv.png"):
-                    pdf.image("logo_smv.png", x=10, y=8, w=30)
 
                 # Título
                 pdf.set_xy(50, 10)
